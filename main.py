775d033c5710a3797a88aeb9482dd97f356494bb
LukeGirvan <126108451+LukeGirvan@users.noreply.github.com>
2023-08-21 15:03:25 +0100

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # Function that creates the fibonacci sequence up to the users input number
 def fibonacci(n):
-    a =0
-    b= 1
+    a = 0
+    b = 1
     x = [a, b]
     if n ==1:
         return [a]
     if n == 2:
-        return [0,1]
+        return [a,b]
     elif n<=0:
         return "error enter valid input"
-    for i in range(2, n+1):
-        c= a+b
-        a= b
-        b= c
-        x.append(c)
-    return x
+    else:
+        for i in range(2, n+1):     # Starts at 2 because the first 2 terms are guaranteed to be 0 and 1 
+            c = a + b
+            a = b
+            b = c
+            x.append(c)
+        return x
 
 
 print(fibonacci(n=int(input())))
